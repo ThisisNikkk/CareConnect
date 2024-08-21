7e7c8eebc5a8f64d111151395decfc2b49e642c2
Nikhil Siwan <developer.nikk@gmail.com>
2024-08-21 19:07:00 +0530

    Sentry Error Fix

--- next.config.mjs
+++ next.config.mjs
@@ -13,8 +13,8 @@ export default withSentryConfig(withSentryConfig(nextConfig, {
 // For all available options, see:
 // https://github.com/getsentry/sentry-webpack-plugin#options
 
-org: "developer-united",
-project: "care-connect",
+org: "thisisnikkk",
+project: "careconnect",
 
 // Only print logs for uploading source maps in CI
 silent: !process.env.CI,
